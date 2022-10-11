dbe16214339c7f7756737a50a9843c882beac59c
VivianB <vivianbacoli@gmail.com>
2022-10-11 15:05:50 -0300

    Initial Commit

---
+++ AtividadeGit/AtividadeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeGitApp.swift
+//  AtividadeGit
+//
+//  Created by Vivian Bacelar on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
