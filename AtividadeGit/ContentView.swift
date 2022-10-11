3d105415d2b6e940ebfd929a94ba0eac69eead43
VivianB <vivianbacoli@gmail.com>
2022-10-11 15:50:19 -0300

    +bF

--- AtividadeGit/ContentView.swift
+++ AtividadeGit/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Amo a Isabella")
+            Text ("Best Friends <3")
+            
             
             
         }
